4d6b2fdbb5f9c6f5316fd46a82d3ec2d9213bcb3
Noe <gnoe8799@gmail.com>
2025-01-02 23:34:26 -0600

    primera adaptacion

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        fondo : "#1D4ED8",
+        fondo2 : "#9333EA"
+      }
+    },
   },
   plugins: [],
 };
