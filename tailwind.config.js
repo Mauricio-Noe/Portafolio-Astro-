69b634bc378b303fbdf47acbda5b68dce39e9b58
Noe <gnoe8799@gmail.com>
2025-01-26 20:52:35 -0600

    cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   theme: {
-    extend:{},
+    extend:{ 
+       scale:{
+
+        '-1': '-1', 
+      },
+    },
   },
   plugins: [],
 };
